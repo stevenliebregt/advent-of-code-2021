b1af0e806a463c0bb256515a9af2d838818f5416
Steven Liebregt <s.liebregt@outlook.com>
2021-12-03 16:00:17 +0100

    init

---
+++ src/day1.rs
@@ -0,0 +1,26 @@
+#[aoc_generator(day1)]
+pub fn input_generator(input: &str) -> Vec<usize> {
+    input
+        .lines()
+        .map(|line| line.parse::<usize>().unwrap())
+        .collect()
+}
+
+#[aoc(day1, part1)]
+pub fn part1(input: &[usize]) -> usize {
+    input
+        .windows(2)
+        .filter(|window| window[0] < window[1])
+        .count()
+}
+
+#[aoc(day1, part2)]
+pub fn part2(input: &[usize]) -> usize {
+    input
+        .windows(3)
+        .map(|group| group.iter().sum())
+        .collect::<Vec<usize>>()
+        .windows(2)
+        .filter(|window| window[0] < window[1])
+        .count()
+}
