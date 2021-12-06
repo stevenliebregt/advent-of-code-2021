b98942b02f91ccf2ed2c5dcf340ed528d732c218
Steven Liebregt <s.liebregt@outlook.com>
2021-12-06 07:59:06 +0100

    finished day 6

--- src/day6.rs
+++ src/day6.rs
@@ -1,70 +1,18 @@
-use std::num::ParseIntError;
-use std::str::FromStr;
+pub fn simulate_lanternfish_school(input: &[usize], days: usize) -> usize {
+    // Split school on timer
+    let mut school: [usize; 9] = [0; 9];
 
-// #[derive(Clone, PartialEq, Debug)]
-// pub struct LanternFish {
-//     internal_timer: usize,
-// }
-//
-// impl LanternFish {
-//     pub fn tick(&mut self) {
-//         self.internal_timer -= 1
-//     }
-//
-//     pub fn is_ready_to_birth(&self) -> bool {
-//         self.internal_timer == 0
-//     }
-//
-//     pub fn reset(&mut self) {
-//         self.internal_timer = 7; // 7 instead of 6 so we can immediately tick down again
-//     }
-// }
-//
-// impl Default for LanternFish {
-//     fn default() -> Self {
-//         Self {
-//             internal_timer: 8
-//         }
-//     }
-// }
-//
-// impl FromStr for LanternFish {
-//     type Err = ParseIntError;
-//
-//     fn from_str(s: &str) -> Result<Self, Self::Err> {
-//         Ok(Self {
-//             internal_timer: s.parse()?
-//         })
-//     }
-// }
-
-pub fn simulate_lanternfish_school(school: &mut Vec<usize>, days: usize) -> usize {
-    let mut queue: usize = 0;
-
-    for _ in 0..days {
-        for fish in school.iter_mut() {
-            if *fish == 0 {
-                queue += 1;
-                *fish = 7
-            }
-
-            *fish -= 1
-        }
-
-        // Spawn new lanternfish and reset queue
-        for _ in 0..queue {
-            school.push(8);
-        }
-
-        queue = 0;
-
-        // println!("After {:>2} {:<4}: {}", day + 1, if day == 0 { "day" } else { "days" }, school.iter()
-        //     .map(|f| f.to_string())
-        //     .collect::<Vec<String>>()
-        //     .join(","));
+    for fish in input {
+        school[*fish] += 1;
     }
 
-    school.len()
+    // Simulate
+    for _ in 0..days {
+        school.rotate_left(1);
+        school[6] += school[8];
+    }
+
+    school.iter().sum()
 }
 
 #[aoc_generator(day6)]
@@ -74,16 +22,12 @@ pub fn input_generator(input: &str) -> Vec<usize> {
 
 #[aoc(day6, part1)]
 pub fn solve_part1(input: &[usize]) -> usize {
-    let mut school = Vec::from(input);
-
-    simulate_lanternfish_school(&mut school, 80)
+    simulate_lanternfish_school(&input, 80)
 }
 
 #[aoc(day6, part2)]
 pub fn solve_part2(input: &[usize]) -> usize {
-    let mut school = Vec::from(input);
-
-    simulate_lanternfish_school(&mut school, 256)
+    simulate_lanternfish_school(&input, 256)
 }
 
 #[cfg(test)]
@@ -109,5 +53,12 @@ mod tests {
         let mut input = input_generator("3,4,3,1,2");
 
         assert_eq!(5934, simulate_lanternfish_school(&mut input, 80))
+    }
+
+    #[test]
+    fn test_part2_example_256days() {
+        let mut input = input_generator("3,4,3,1,2");
+
+        assert_eq!(26984457539, simulate_lanternfish_school(&mut input, 256))
     }
 }
